638451d66d18fa066ba041aa9eac9bc34620d689
Soham Chaudhary <chaudharisoham041@gmail.com>
2026-02-15 10:54:28 +0530

    solved

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // FORCE HTTP://LOCALHOST:5000/API
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
   headers: {
     'Content-Type': 'application/json',
   },
